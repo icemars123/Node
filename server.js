2ead9eba48dd5647fc923fd2fa8543180c72f5f2
icemars123 <gavinniubi@gmail.com>
2018-07-31 17:13:21 +1000

    new

--- server.js
+++ server.js
@@ -25,32 +25,53 @@ app.get('/messages', (req, res) => {
     })
 })
 
-app.post('/messages', (req, res) => {
+// async and await
+app.post('/messages', async (req, res) => {
     var message = new Message(req.body)
 
-    //promise
-    //If error, then to catch. If no error, then to save. -> and try to find badword(promise).
-    // if find it, then to remove. if not find it, then to emit.
-    message.save()
-    .then(() => {
-        console.log('saved')
-        return Message.findOne({ message: 'badword' })
-    })
-    .then( censored => {
-        if (censored) {
-            console.log('censored words found', censored)
-            return Message.remove({ _id: censored.id })
-        }
-        io.emit('message', req.body)
-        res.sendStatus(200)
-    })
-    .catch((err) => {
-        res.sendStatus(500)
-        return console.error(err)     
-    })
+    var savedMessage = await message.save()
+    
+    console.log('saved')
 
+    var censored =  await Message.findOne({message: 'badword'})
+
+    if (censored)  
+        await Message.remove({ _id: censored.id })
+    else
+        io.emit('message', req.body)
+    res.sendStatus(200)
+    
+    // .catch((err) => {
+    //     res.sendStatus(500)
+    //     return console.error(err)     
+    // })
 })
 
+// Promises and sync
+// app.post('/messages', (req, res) => {
+//     var message = new Message(req.body)
+
+//     //promise
+//     //If error, then to catch. If no error, then to save. -> and try to find badword(promise).
+//     // if find it, then to remove. if not find it, then to emit.
+//     message.save()
+//         .then(() => {
+//             console.log('saved')
+//             return Message.findOne({ message: 'badword' })
+//         })
+//         .then(censored => {
+//             if (censored) {
+//                 console.log('censored words found', censored)
+//                 return Message.remove({ _id: censored.id })
+//             }
+//             io.emit('message', req.body)
+//             res.sendStatus(200)
+//         })
+//         .catch((err) => {
+//             res.sendStatus(500)
+//             return console.error(err)
+//         })
+// })
 
 io.on('connection', (socket) => {
     console.log('a user connected')
