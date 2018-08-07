37fefcc8e091d87509c20bd489ce0d610dfa09e0
icemars123 <gavinniubi@gmail.com>
2018-08-07 13:47:41 +1000

    app.post wth promises and sync updated

--- server.js
+++ server.js
@@ -35,57 +35,60 @@ app.get('/messages/:user', (req, res) => {
 //async and await
 //async, only until all await to go through, the await can commit. await just be promise.
 //if the below is not async and await, then anything will be saved, but nothing'll be showed at the same time. In the back, nothing will be removed.
-app.post('/messages', async (req, res) => {
+// app.post('/messages', async (req, res) => {
 
-    try {
-        var message = new Message(req.body)
+//     try {
+//         var message = new Message(req.body)
 
-        var savedMessage =  await message.save()
+//         var savedMessage =  await message.save()
 
-        console.log('saved')
+//         console.log('saved')
 
-        var censored =  await Message.findOne({ message: 'badword' })
+//         var censored =  await Message.findOne({ message: 'badword' })
 
-        if (censored)
-            await Message.remove({ _id: censored.id })
-        else
-            io.emit('message', req.body)
-        res.sendStatus(200)
-    } catch (error) {
-        res.sendStatus(500)
-        return console.error(error) 
-    }finally{
-        //logger.log('message post called')
-        console.log('message post called')
-    }
+//         if (censored)
+//             await Message.remove({ _id: censored.id })
+//         else
+//             io.emit('message', req.body)
+//         res.sendStatus(200)
+//     } catch (error) {
+//         res.sendStatus(500)
+//         return console.error(error) 
+//     }finally{
+//         //logger.log('message post called')
+//         console.log('message post called')
+//     }
 
-})
+// })
 
 // Promises and sync
-// app.post('/messages', (req, res) => {
-//     var message = new Message(req.body)
+app.post('/messages', (req, res) => {
+    var message = new Message(req.body)
 
-//     //promise
-//     //If error, then to catch. If no error, then to save. -> and try to find badword(promise).
-//     // if find it, then to remove. if not find it, then to emit.
-//     message.save()
-//         .then(() => {
-//             console.log('saved')
-//             return Message.findOne({ message: 'badword' })
-//         })
-//         .then(censored => {
-//             if (censored) {
-//                 console.log('censored words found', censored)
-//                 return Message.remove({ _id: censored.id })
-//             }
-//             io.emit('message', req.body)
-//             res.sendStatus(200)
-//         })
-//         .catch((err) => {
-//             res.sendStatus(500)
-//             return console.error(err)
-//         })
-// })
+    //promise
+    //If error, then to catch. If no error, then to save. -> and try to find badword(promise).
+    // if find it, then to remove. if not find it, then to emit.
+    // note:
+    // if post some content, then save it to the DB, then to find whether the badword is or not.
+    // If found "badword", then remove it from the DB, then io.emit message to the page, and send the status
+    message.save()
+        .then(() => {
+            console.log('saved')
+            return Message.findOne({ message: 'badword' })
+        })
+        .then(censored => {
+            if (censored) {
+                console.log('censored words found', censored)
+                return Message.remove({ _id: censored.id })
+            }
+            io.emit('message', req.body)
+            res.sendStatus(200)
+        })
+        .catch((err) => {
+            res.sendStatus(500)
+            return console.error(err)
+        })
+})
 
 io.on('connection', (socket) => {
     console.log('a user connected')
